1091c6db15afe8218fdef1d3e992743dd9005ecf
underkruk <underkruk@gmail.com>
2016-12-10 14:36:57 +0200

    add HW 2016_04_12 (getCustomFormat)

--- 2016_12_04/1_exercise/js/common.js
+++ 2016_12_04/1_exercise/js/common.js
@@ -1,3 +1,50 @@
-/**
- * Created by User on 2016/12/08.
- */
+(function(){
+
+
+	Date.prototype.getCustomFormat = function( dateFormat ){
+
+		return format( this, dateFormat );
+		
+	}
+
+
+	function format(date, dateFormat){
+		
+		let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+		let objFormat = { 
+
+			YYYY : date.getFullYear(),
+			YY 	 : date.getFullYear().toString().slice(2),
+			MM	 : parseInt(date.getMonth()) + 1,
+			MMMM : monthNames[date.getMonth()],
+			DD	 : date.getDate(),
+			HH	 : date.getHours(),
+			mm   : date.getMinutes(),
+			ss   : date.getSeconds()
+		};
+
+		
+		let dateTime = dateFormat.trim().includes(" ") ? dateFormat.trim().split(" ") : [dateFormat.trim()],
+						dateTostr = "";
+
+		dateTime.forEach(function(el, iter){
+
+			let delimiter = iter == 0 ? "-" : ":";
+			
+				dateTostr += dateTime[iter].split(delimiter).map(e => objFormat[e]).join(delimiter) + " ";
+
+		})
+
+		 	
+
+		return dateTostr;
+	}
+
+
+let someDate = new Date(2017, 3, 23, 15, 45, 49);
+
+console.log ( someDate.getCustomFormat ( "YY-MMMM-DD HH:mm:ss" ) );
+})()
+
+
